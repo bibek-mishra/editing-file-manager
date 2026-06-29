7055dfa5bfc1eae55689de6f4547a8953ef837b2
Bibek Mishra <mishrabibek874@gmail.com>
2026-06-29 15:59:39 +0530

    adding sub folders

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 import os 
 
+folders = ["imgs","vids","audios","sfx","projectfile","random"]
+
 while True:
     project_name = input("> ")
-    if os.path.exists(project_name):
-        print("You can't use this file name.")
-        continue
-    else:
+    if not os.path.exists(project_name):
         os.mkdir(project_name)
         break 
+    else:
+        print("You can't use this file name.")
+        continue
+
+for f in folders:
+    path = os.path.join(project_name,f)
+
+    if not os.path.exists(path):
+        os.mkdir(path)
+        print(f"{f} created")
+    else:
+        print(f"{f} already exists")
